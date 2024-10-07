e9c6c0bae557416a88a54b4a075e704266156471
jissssu <113964353+jissssu@users.noreply.github.com>
2024-10-07 15:57:35 +0900

    fix: 에러 해결

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,15 @@
 import { StyledInput } from "./styles";
 
+function handleInput(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
 function Input() {
   return (
     <>
       <h3> 입력</h3>
-      <StyledInput />
+      <StyledInput onClick={handleInput}/>
     </>
   );
 }
